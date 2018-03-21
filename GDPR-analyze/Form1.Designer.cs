0dd75a99c0174cd25be079ce93edc7a77865d5d4
ioanmitroi <ioanmitroi@godrivecarbox.com>
2018-03-21 14:17:02 +0200

    material design

--- GDPR-analyze/Form1.Designer.cs
+++ GDPR-analyze/Form1.Designer.cs
@@ -28,13 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+			this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+			this.materialCheckBox1 = new MaterialSkin.Controls.MaterialCheckBox();
+			this.materialCheckBox2 = new MaterialSkin.Controls.MaterialCheckBox();
+			this.SuspendLayout();
+			// 
+			// materialLabel1
+			// 
+			this.materialLabel1.AutoSize = true;
+			this.materialLabel1.Depth = 0;
+			this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+			this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.materialLabel1.Location = new System.Drawing.Point(148, 202);
+			this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+			this.materialLabel1.Name = "materialLabel1";
+			this.materialLabel1.Size = new System.Drawing.Size(136, 24);
+			this.materialLabel1.TabIndex = 0;
+			this.materialLabel1.Text = "materialLabel1";
+			// 
+			// materialCheckBox1
+			// 
+			this.materialCheckBox1.AutoSize = true;
+			this.materialCheckBox1.Depth = 0;
+			this.materialCheckBox1.Font = new System.Drawing.Font("Roboto", 10F);
+			this.materialCheckBox1.Location = new System.Drawing.Point(152, 144);
+			this.materialCheckBox1.Margin = new System.Windows.Forms.Padding(0);
+			this.materialCheckBox1.MouseLocation = new System.Drawing.Point(-1, -1);
+			this.materialCheckBox1.MouseState = MaterialSkin.MouseState.HOVER;
+			this.materialCheckBox1.Name = "materialCheckBox1";
+			this.materialCheckBox1.Ripple = true;
+			this.materialCheckBox1.Size = new System.Drawing.Size(181, 30);
+			this.materialCheckBox1.TabIndex = 1;
+			this.materialCheckBox1.Text = "materialCheckBox1";
+			this.materialCheckBox1.UseVisualStyleBackColor = true;
+			// 
+			// materialCheckBox2
+			// 
+			this.materialCheckBox2.AutoSize = true;
+			this.materialCheckBox2.Depth = 0;
+			this.materialCheckBox2.Font = new System.Drawing.Font("Roboto", 10F);
+			this.materialCheckBox2.Location = new System.Drawing.Point(140, 275);
+			this.materialCheckBox2.Margin = new System.Windows.Forms.Padding(0);
+			this.materialCheckBox2.MouseLocation = new System.Drawing.Point(-1, -1);
+			this.materialCheckBox2.MouseState = MaterialSkin.MouseState.HOVER;
+			this.materialCheckBox2.Name = "materialCheckBox2";
+			this.materialCheckBox2.Ripple = true;
+			this.materialCheckBox2.Size = new System.Drawing.Size(181, 30);
+			this.materialCheckBox2.TabIndex = 2;
+			this.materialCheckBox2.Text = "materialCheckBox2";
+			this.materialCheckBox2.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(534, 408);
+			this.Controls.Add(this.materialCheckBox2);
+			this.Controls.Add(this.materialCheckBox1);
+			this.Controls.Add(this.materialLabel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
         }
 
-        #endregion
-    }
+		#endregion
+
+		private MaterialSkin.Controls.MaterialLabel materialLabel1;
+		private MaterialSkin.Controls.MaterialCheckBox materialCheckBox1;
+		private MaterialSkin.Controls.MaterialCheckBox materialCheckBox2;
+	}
 }
 
--- GDPR-analyze/Form1.cs
+++ GDPR-analyze/Form1.cs
@@ -8,14 +8,29 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using MaterialSkin;
+using MaterialSkin.Animations;
+using MaterialSkin.Controls;
+
 namespace GDPR_analyze
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
         public Form1()
         {
             InitializeComponent();
-        }
+
+			MaterialSkinManager managerSkinManager = MaterialSkinManager.Instance;
+			managerSkinManager.AddFormToManage(this);
+			managerSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+
+			managerSkinManager.ColorScheme = new ColorScheme(
+				Primary.Blue400, Primary.Blue700,
+				Primary.Blue600,Accent.Cyan400,
+				TextShade.WHITE
+				);
+
+		}
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -24,5 +39,10 @@ namespace GDPR_analyze
 
 			//mersi erik 2
         }
-    }
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
 }
